bee53816887e7abf038c4c7a290a3c6d3efb2781
singh <singh@KINGLEBRON>
2025-03-20 09:50:58 +1300

    Add project files.

---
+++ InsuranceApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace InsuranceApp
+{
+    internal class Program
+    {
+        // Global Variables
+
+        int userchoice = 0;
+
+        List<string> Laptop = new List<string>();
+        List<string> Desktop = new List<string>();
+        List<string> Other = new List<string>();
+
+        Console.Writeline() =
+    }
+}
