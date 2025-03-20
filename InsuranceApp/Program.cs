7390361d6c27cfa6712fe52b65f35d5bd6a115d1
Manvir Singh <manvsing2@stjohns.school.nz>
2025-03-21 12:26:58 +1300

    V1.0

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -1,18 +1,21 @@
-﻿using System.Diagnostics;
-using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
 
 namespace InsuranceApp
 {
     internal class Program
     {
         // Global Variables
+        static int userchoice = 0;
+        static List<string> Laptop = new List<string>();
+        static List<string> Desktop = new List<string>();
+        static List<string> Other = new List<string>();
+        static List<string> CATEGORY = new List<string>() { "Computer", "Desktop", "other" };
+        // Constant variables
 
-        int userchoice = 0;
+        // Methods and Functions
 
-        List<string> Laptop = new List<string>();
-        List<string> Desktop = new List<string>();
-        List<string> Other = new List<string>();
-
-        Console.Writeline() =
+        
     }
 }
+
