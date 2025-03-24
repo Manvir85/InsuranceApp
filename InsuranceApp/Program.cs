31f765fafddba3effcf221714f587d9b0aea79a1
Manvir Singh <manvsing2@stjohns.school.nz>
2025-03-24 13:10:02 +1300

    V2.0

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -6,16 +6,20 @@ namespace InsuranceApp
     internal class Program
     {
         // Global Variables
-        static int userchoice = 0;
-        static List<string> Laptop = new List<string>();
-        static List<string> Desktop = new List<string>();
-        static List<string> Other = new List<string>();
-        static List<string> CATEGORY = new List<string>() { "Computer", "Desktop", "other" };
-        // Constant variables
+        static List<string> CATEGORY = new List<string>() { "Laptop", "Desktop", "Other"};
+
+        static int laptopCounter = 0, desktopCounter = 0, otherCounter = 0;
+        static string priciestDeviceName = "";
+        static float insuranceCost = 0, priciestDevice = 0; 
+
 
         // Methods and Functions
 
-        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start of program");
+        }
+
     }
 }
 
