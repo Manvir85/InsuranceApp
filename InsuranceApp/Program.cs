0cacf3ef2a44cc439ece1d07c9ee0e1aa671c8e4
Manvir Singh <manvsing2@stjohns.school.nz>
2025-03-28 12:17:13 +1300

    V3.0

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -1,25 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace InsuranceApp;
 
-namespace InsuranceApp
+class Program
 {
-    internal class Program
+    // Global Variables
+    static int laptopCounter = 0, desktopCounter = 0, otherCounter = 0;
+    static string mostExpensiveDevice = "";
+    static float totalInsuranceCost = 0, priciestDevice = 0;
+
+
+    // Constant Variables
+
+    static List<string> CATEGORY = new List<string>() { "Laptop\n", "Desktop\n", "Other\n" };
+
+    // Methods and Functions
+    static void InputDeviceInfo()
     {
-        // Global Variables
-        static List<string> CATEGORY = new List<string>() { "Laptop", "Desktop", "Other"};
-
-        static int laptopCounter = 0, desktopCounter = 0, otherCounter = 0;
-        static string priciestDeviceName = "";
-        static float insuranceCost = 0, priciestDevice = 0; 
-
-
-        // Methods and Functions
-
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Start of program");
-        }
 
     }
-}
+    static void OneDevice()
+    {
+        // Local Variables
 
+        string deviceName;
+        int category, numDevice;
+        float devicePrice, deviceInsurance = 0; ;
+
+        // Input  the device name
+        Console.WriteLine("Enter the device name:");
+        deviceName = Console.ReadLine();
+
+        Console.WriteLine("Enter the number of devices:");
+        numDevice = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the cost per device:");
+        float deviceCost = float.Parse(Console.ReadLine());
+
+        string categoryMenu = "Select the device category:\n";
+        int categoryNumber = 0;
+
+        foreach (var cat in CATEGORY)
+        {
+
+            categoryNumber++;
+            categoryMenu += $"{categoryNumber}.{cat}";
+        }
+
+        Console.WriteLine(categoryMenu);
+        Console.ReadLine();
+
+
+
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        OneDevice();
+    }
+}
