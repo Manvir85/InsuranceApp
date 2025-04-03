0ce64ccb20995fd59e06b91b6465d8d0db2f2512
Manvir Singh <manvsing2@stjohns.school.nz>
2025-04-04 11:47:11 +1300

    V6.0

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -1,117 +1,105 @@
-﻿namespace InsuranceApp;
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsuranceApp;
 
 class Program
 {
+    static Random random = new Random();
+
     // Global Variables
     static int laptopCounter = 0, desktopCounter = 0, otherCounter = 0;
     static string mostExpensiveDevice = "";
     static float totalInsuranceCost = 0, priciestDevice = 0;
 
-
     // Constant Variables
+    static List<string> CATEGORY = new List<string>() { "Laptop", "Desktop", "Other" };
 
-    static List<string> CATEGORY = new List<string>() { "Laptop\n", "Desktop\n", "Other\n" };
+    // Method to generate a unique device code
+    static string GenerateDeviceCode()
+    {
+        return $"DEV{random.Next(100, 999)}@{random.Next(10, 99)}";
+    }
 
     // Methods and Functions
-    static void InputDeviceInfo()
-    {
-
-    }
     static void OneDevice()
     {
         Console.WriteLine("░█░█░█▀▀░█░░░█▀▀░█▀█░█▄█░█▀▀░░░▀█▀░█▀█░░░▀█▀░█▀█░█▀▀░█░█░█▀▄░█▀█░█▀█░█▀▀░█▀▀\r\n░█▄█░█▀▀░█░░░█░░░█░█░█░█░█▀▀░░░░█░░█░█░░░░█░░█░█░▀▀█░█░█░█▀▄░█▀█░█░█░█░░░█▀▀\r\n░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░▀░░▀▀▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀▀\r\n░█▀█░█▀█░█▀█                                                                \r\n░█▀█░█▀▀░█▀▀                                                                \r\n░▀░▀░▀░░░▀░░                                                                ");
-        // Local Variables
-
-        string deviceName;
-        int category, numDevice;
-        float devicePrice, deviceInsurance = 0; ;
-
-        // Input  the device name
+        
+        // Input the Device name
         Console.WriteLine("Enter the device name:");
-        deviceName = Console.ReadLine();
+        string deviceName = Console.ReadLine();
+        string deviceCode = GenerateDeviceCode();
 
         Console.WriteLine("Enter the number of devices:");
-        numDevice = Convert.ToInt32(Console.ReadLine());
+        int numDevice = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Enter the cost per device:");
         float deviceCost = float.Parse(Console.ReadLine());
 
-        string categoryMenu = "Select the device category:\n";
-        int categoryNumber = 0;
-
-        foreach (var cat in CATEGORY)
+        Console.WriteLine("Select the device category:");
+        for (int i = 0; i < CATEGORY.Count; i++)
         {
-
-            categoryNumber++;
-            categoryMenu += $"{categoryNumber}.{cat}";
+            Console.WriteLine($"{i + 1}. {CATEGORY[i]}");
         }
-
-        Console.WriteLine(categoryMenu);
-        Console.ReadLine();
+        int category = Convert.ToInt32(Console.ReadLine());
 
         // Adds the user input to a counter for the appropriate category
+        if (category == 1) laptopCounter += numDevice;
+        else if (category == 2) desktopCounter += numDevice;
+        else otherCounter += numDevice;
 
-        if (categoryNumber == 1)
-        {
-            laptopCounter += numDevice;
-
-        }
-        else if (categoryNumber == 2)
-        {
-            desktopCounter += numDevice;
-
-        }
-        else
-        {
-            otherCounter += numDevice;
-
-        }
-
+        // Calculate insurance cost with discount
+        float deviceInsurance = 0;
         if (numDevice > 5)
         {
             deviceInsurance += 5 * deviceCost;
-
             deviceInsurance += (numDevice - 5) * deviceCost * 0.9f;
         }
-
         else
         {
             deviceInsurance += numDevice * deviceCost;
         }
 
+        totalInsuranceCost += deviceInsurance;
 
-        Console.WriteLine("-----------------------------------------------\n");
+        if (deviceInsurance > priciestDevice)
+        {
+            priciestDevice = deviceInsurance;
+            mostExpensiveDevice = $"{deviceName} @ ${priciestDevice:F2}";
+        }
+
+        Console.WriteLine("-----------------------------------------------");
         // Display the Insurance Cost
-        Console.WriteLine($"{deviceName}");
-        Console.WriteLine($"Total cost for {numDevice} x {deviceName} is = {deviceInsurance:F2} (with insurance)");
-
+        Console.WriteLine($"{deviceName} ({deviceCode})");
+        Console.WriteLine($"Total cost for {numDevice} x {deviceName} is = ${deviceInsurance:F2} (with insurance)");
+        
         // Display depreciation
-        Console.WriteLine($"Depreciation over 6 months:\n");
+        Console.WriteLine("\nDepreciation over 6 months:");
 
         for (int month = 1; month <= 6; month++)
         {
             deviceCost *= 0.95f; // 5% depreciation per month
-
-            Console.WriteLine($"Month: {month}\tValue Lost: {deviceCost:F2}\n");
+            Console.WriteLine($"Month: {month}\tValue: ${deviceCost:F2}");
         }
 
-
-
-        Console.WriteLine($"CATEGORY: {CATEGORY[categoryNumber - 1]}");
-
+        Console.WriteLine($"CATEGORY: {CATEGORY[category - 1]}");
     }
-
-
-    }
-
-
-
-    
-
-
 
     static void Main(string[] args)
     {
-        OneDevice();
+        string proceed = "";
+        while (proceed.ToLower() != "stop")
+        {
+            OneDevice();
+            Console.WriteLine("Press Enter to add another device or type 'Stop' to finish.");
+            proceed = Console.ReadLine();
+        }
+
+        Console.WriteLine($"\nThe number of laptops: {laptopCounter}");
+        Console.WriteLine($"The number of desktops: {desktopCounter}");
+        Console.WriteLine($"The number of other devices: {otherCounter}");
+        Console.WriteLine($"\nThe total value for insurance: ${totalInsuranceCost:F2}");
+        Console.WriteLine($"The most expensive device - {mostExpensiveDevice}");
     }
 }
